91b33e2f4e59fc673f23fad9c4682b35f6b3ab02
Anis <imarahmandev@gmail.com>
2024-03-24 00:00:01 +0700

    Setup Tailwind css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
+<script>
+	import '../app.pcss';
+</script>
+
 <svelte:head>
-    <title>Sveltekit Demo APP</title>
-    </svelte:head>  
+	<title>Sveltekit Demo APP</title>
+</svelte:head>
 
 <slot />
