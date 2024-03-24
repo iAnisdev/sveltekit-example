bcd21bc6a4261365c70d77f40391034a2edfe6fd
Anis <imarahmandev@gmail.com>
2024-03-25 01:58:31 +0700

    Update Layout to work as data bridge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script>
 	import '../app.pcss';
+	import TopLevelHeader from '../components/TopLevelHeader.svelte';
+	import { ticker } from '../stores/store';
+
+	export let data;
+	
+	$: if (data) {
+		ticker.set(data.ticker);
+	}
 </script>
 
 <svelte:head>
 	<title>Sveltekit Demo APP</title>
 </svelte:head>
 
+<TopLevelHeader />
 <slot />
