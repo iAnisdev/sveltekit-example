8d0401c30548593a26a44b44012844ec44320980
Anis <imarahmandev@gmail.com>
2024-03-25 02:12:44 +0700

    homepage setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,22 @@
-<p>Home page</p>
+<script>
+	import { Tabs, TabItem } from 'flowbite-svelte';
+</script>
+
+<div class="container mx-auto mt-4">
+	<Tabs>
+		<TabItem open title="Blocks">
+			<p class="text-sm text-gray-500 dark:text-gray-400">
+				<b>Blocks:</b>
+		</TabItem>
+		<TabItem title="Transactions">
+			<p class="text-sm text-gray-500 dark:text-gray-400">
+				<b>Latest Transactions:</b>
+			</p>
+		</TabItem>
+		<TabItem title="Unconfirmed Transactions">
+			<p class="text-sm text-gray-500 dark:text-gray-400">
+				<b>Unconfirmed Transactions:</b>
+			</p>
+		</TabItem>
+	</Tabs>
+</div>
